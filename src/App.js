a89e9fa8e360f40b2a62627a1c0b1c632aea4d75
venkateshg05 <venkatesh_gopal@outlook.com>
2022-08-11 20:48:11 -0500

    (clean)

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.py4e.com/api/films/");
+    const data = await response.json();
+    const transformedData = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transformedData);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
